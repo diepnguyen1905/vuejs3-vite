28294865c79cec8439c0b12aa1020b83251346e8
Diep Nguyen <diepnv@Diepnv.local>
2023-02-18 20:59:53 +0700

    init vuejs3 and vit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<style scoped>
+</style>
+
+
+<template>
+  <h1> Start Vue3js+ vite </h1>
+
+<!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script>
+import {
+    ref,
+    reactive
+} from 'vue'
+
+const Data = reactive({
+    number: 0,
+    title: "This is title"
+})
+</script>
+
+<template>
+<h1>counter: {{ Data.counter }}</h1>
+</template>
+
+<style scoped>
+</style>
